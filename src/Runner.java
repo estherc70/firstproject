500e8255afc07d7e399148f6fffc0f9f57343878
BT_1N3_32 <sample@bths.edu>
2024-10-25 13:10:26 -0400

    added more print statements

--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,13 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("I hate AP Chem!!!!!!!!!!!");
         System.out.println("Chemistry sucks so much!!!");
-        System.out.println("APCSA is so much better");
+        System.out.println("APCSA is so much better like so much!");
+        double average = 78;
+        if (average>60) {
+            System.out.println("You hate chem but you are passing chem!");
+        }
+        else {
+            System.out.println("I hate chem");
+        }
     }
 }

fa7c65c743106ba5841a63a591c1c9bce2813647
BT_1N3_32 <sample@bths.edu>
2024-10-25 13:08:08 -0400

    added more hate for chem statements

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("I hate AP Chem!!!!!!!!!!!!!!");
+        System.out.println("I hate AP Chem!!!!!!!!!!!");
+        System.out.println("Chemistry sucks so much!!!");
+        System.out.println("APCSA is so much better");
     }
 }
